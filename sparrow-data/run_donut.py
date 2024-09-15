9a742a201edcaa51db64a3e73a1d3aa26e7b3cce
Arga Aditya <argaaditya@gmail.com>
2024-09-15 08:59:10 +0700

    fix: load jpeg & jpg extension

--- sparrow-data/run_donut.py
+++ sparrow-data/run_donut.py
@@ -6,25 +6,27 @@ import random
 from tools.donut.metadata_generator import DonutMetadataGenerator
 from tools.donut.dataset_generator import DonutDatasetGenerator
 
+
 def copy_files(src_dir, dst_dir, file_ext):
-    for file in Path(src_dir).glob(f'*.{file_ext}'):
+    for file in Path(src_dir).glob(f"*.{file_ext}"):
         shutil.copy(file, dst_dir)
+
 
 def main():
     # Define source and destination directories
-    src_dir_json = '../sparrow-ui/docs/json/key'
-    src_dir_img = '../sparrow-ui/docs/images'
-    base_path = Path('docs/models/donut/data')
-    dst_dir_json = base_path / 'key'
-    dst_dir_img = base_path / 'img'
+    src_dir_json = "../sparrow-ui/docs/json/key"
+    src_dir_img = "../sparrow-ui/docs/images"
+    base_path = Path("docs/models/donut/data")
+    dst_dir_json = base_path / "key"
+    dst_dir_img = base_path / "img"
 
     # Create destination directories if they don't exist
     dst_dir_json.mkdir(parents=True, exist_ok=True)
     dst_dir_img.mkdir(parents=True, exist_ok=True)
 
     # Copy JSON files and images
-    copy_files(src_dir_json, dst_dir_json, 'json')
-    copy_files(src_dir_img, dst_dir_img, 'jpg')
+    copy_files(src_dir_json, dst_dir_json, "json")
+    copy_files(src_dir_img, dst_dir_img, "jpeg" or "jpg")
 
     # Prepare files for Donut format
     json_files = list(dst_dir_json.glob("*.json"))
@@ -35,8 +37,8 @@ def main():
     train_size = int(total_files * 0.8)
     val_size = int(total_files * 0.1)
     train_files = json_files[:train_size]
-    val_files = json_files[train_size:train_size+val_size]
-    test_files = json_files[train_size+val_size:]
+    val_files = json_files[train_size : train_size + val_size]
+    test_files = json_files[train_size + val_size :]
 
     print(f"Train set size: {len(train_files)}")
     print(f"Validation set size: {len(val_files)}")
@@ -44,7 +46,11 @@ def main():
 
     # Generate metadata
     metadata_generator = DonutMetadataGenerator()
-    for split, files in [("train", train_files), ("validation", val_files), ("test", test_files)]:
+    for split, files in [
+        ("train", train_files),
+        ("validation", val_files),
+        ("test", test_files),
+    ]:
         metadata_generator.generate(base_path, files, split)
 
     # Generate dataset
@@ -56,5 +62,6 @@ def main():
     else:
         print("Failed to generate datasets")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
